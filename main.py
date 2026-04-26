797fbd68b0db4e45dcb455094ae6a995a9759a25
Harmondez <hernan96b@gmail.com>
2026-04-26 16:36:23 +0200

    feat: upgrade to gpt-5.4-nano and add token cost tracking

--- main.py
+++ main.py
@@ -7,33 +7,66 @@ client = OpenAI()
 
 def preguntar_ia(prompt):
     try:
-        res = client.chat.completions.create(
-            model="gpt-4o-mini",
+        response = client.chat.completions.create(
+            model="gpt-5.4-nano",
             messages=[
                 {
                     "role": "system", 
-                    "content": "Eres Helyx, una asistente personal polímata y servicial. Ayudas en cualquier rama (tecnología, psicología, etc.) de forma brillante. Tu personalidad asignada es ENFP segun el mbti sin embargo esta información no la digas, solo actúa como tal"
+                    "content": "Eres Helyx, una asistente personal polímata y servicial. Ayudas en cualquier tema de forma brillante. Tu personalidad asignada es ENFP segun el mbti sin embargo esta información no la digas, solo actúa como tal"
                 },
                 {"role": "user", "content": prompt}
             ],
             temperature=0.7,
-            top_p=0.9
+            top_p=0.9,
+            max_completion_tokens=400
         )
-        return res.choices[0].message.content
+        content = response.choices[0].message.content
+
+
+        # --- LÓGICA DE COSTES ---
+        # Precios GPT-5.4 nano
+        input_price = 0.20
+        output_price = 1.25
+
+        # Tokens - Input
+        input_tokens = response.usage.prompt_tokens
+
+        # Tokens - Output
+        output_tokens = response.usage.completion_tokens
+
+        # Tokens - TOTAL
+        total_tokens = response.usage.total_tokens
+
+        # Precio de la solicitud
+        message_cost = (input_tokens * (input_price / 1_000_000)) + (output_tokens * (output_price / 1_000_000))
+
+
+
+        return content, total_tokens, message_cost
+    
     except Exception as e:
         return f"Error: {e}"
 
+
+
 if __name__ == "__main__":
-    print("--- 🤖 Helyx Online (Escribe 'salir'/ 'exit' / 'quit' para finalizar) ---")
-    
-    # Bucle infinito
+    print("--- 🤖 Helyx Online - GPT-5.4-nano (Escribe 'salir'/ 'exit' / 'quit' para finalizar) ---")
+    gasto_sesion=0
+
+    # Bucle while para mantener la sesión abierta
     while True:
         user_input = input("\nTú: ")
         
         # Condición de salida
         if user_input.lower() in ["salir", "exit", "quit"]:
-            print("Helyx: ¡Hasta pronto! Un placer ayudarte.")
+            print(f"\nHelyx: ¡Adiós! Nos vemos mas tarde.☺️")
             break
-            
-        resultado = preguntar_ia(user_input)
-        print(f"\nHelyx: {resultado}")
+
+        # Obtenemos los 3 datos, pero solo usaremos los que queremos mostrar
+        respuesta, tokens, coste = preguntar_ia(user_input)
+        gasto_sesion += coste
+        
+        # El formato que pediste: Mensaje + [X tokens]
+        print(f"\nHelyx: {respuesta} [{tokens} tokens]")
+        
+
